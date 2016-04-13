52d7728a7f716dbded072a59801a3be6bb7b3ae2
denodaeus <bobby.smith@gmail.com>
2016-04-13 08:16:17 -0400

    Test Case Manager - Failed

---
+++ src/test-case-manager/TODOS-63.spec.ts
@@ -0,0 +1,21 @@
+import {
+  it,
+  describe,
+  expect,
+  beforeEach,
+} from 'angular2/testing';
+
+describe('TODOS-60', () => {
+
+ describe('Check for the correct contents of the header', () => {
+    it("should say Hello QualityJam", (done) => {
+      expect(true).toEqual('header does not say Hello QualityJam!');
+      done();
+    });
+    it("should contain an !", (done) => {
+      //expect(true).toEqual('header does not contain a !');
+      done();
+    });
+  });
+
+});
