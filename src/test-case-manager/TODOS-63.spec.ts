b2e08bfafede682799c5f7d12a8ba1464c295e4f
denodaeus <bobby.smith@gmail.com>
2016-04-13 08:18:45 -0400

    Test Run #27

---
+++ src/test-case-manager/TODOS-63.spec.ts
@@ -0,0 +1,21 @@
+import {
+  it,
+  describe,
+  expect,
+  beforeEach,
+} from 'angular2/testing';
+
+describe('TODO-63', () => {
+
+ describe('Check for the correct contents of the header', () => {
+    it("should say Hello QualityJam!", (done) => {
+      expect(true).toEqual(false);
+      done();
+    });
+    it("should contain an !", (done) => {
+      expect(true).toEqual(true);
+      done();
+    });
+  });
+
+});
