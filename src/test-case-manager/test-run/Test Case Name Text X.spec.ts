fd4a8dfdde7cfe8d34ac6877917578cf3afdf8ad
Bobby Smith <bobby.smith@gmail.com>
2016-07-13 13:39:13 -0400

    Test Case Results for Test Case Name Text X

---
+++ src/test-case-manager/test-run/Test Case Name Text X.spec.ts
@@ -0,0 +1,10 @@
+
+      describe('Test Case Name Text X', function(Inputs) {
+        
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['1'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      });    
+    
