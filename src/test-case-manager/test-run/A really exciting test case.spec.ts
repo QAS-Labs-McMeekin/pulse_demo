cf9790c06d79c30513213335fd4ae155671d415b
Bobby Smith <bobby.smith@gmail.com>
2016-07-13 23:48:36 -0400

    Test Case Results for A really exciting test case

---
+++ src/test-case-manager/test-run/A really exciting test case.spec.ts
@@ -0,0 +1,45 @@
+
+      describe('A really exciting test case', function(Inputs) {
+        
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['1'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['2'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['3'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['4'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['5'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['6'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['7'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      it('should Step Description Text', function(Done) {
+        expect(Inputs['Test Run']['8'].result).to.eql('Step Excepted result Text');
+        Done();
+      });
+    
+      });    
+    
