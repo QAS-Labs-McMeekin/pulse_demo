847b9d294ffd3045e8006869853b51caaca2113c
Bobby Smith <bobby.smith@gmail.com>
2016-08-24 11:25:46 -0400

    Test Case Results for Add a user

---
+++ src/test-case-manager/test-run/Add a user.spec.ts
@@ -0,0 +1,15 @@
+
+      describe('Add a user', function(Inputs) {
+        
+      it('should Step Description', function(Done) {
+        expect(Inputs['Test Run']['1'].result).to.eql('Step Excepted result');
+        Done();
+      });
+    
+      it('should Step Description', function(Done) {
+        expect(Inputs['Test Run']['2'].result).to.eql('Step Excepted result');
+        Done();
+      });
+    
+      });
+    
