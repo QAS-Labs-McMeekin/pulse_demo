824553afad30d611c649bd5fa4647d56b313a1d4
Bobby Smith <bobby.smith@gmail.com>
2017-01-17 23:38:58 +0700

    Update Some other test case 24.spec.ts

--- src/test-case-manager/test-run/Some other test case 24.spec.ts
+++ src/test-case-manager/test-run/Some other test case 24.spec.ts
@@ -2,17 +2,17 @@
       describe('Some other test case 24', function(done) {
         
       it('should Step', function(done) {
-        expect('failed').to.eql('Result');
+        expect('Result').to.eql('Result');
         done();
       });
     
       it('should Another Step', function(done) {
-        expect('failed').to.eql('Another Result');
+        expect('Another Result').to.eql('Another Result');
         done();
       });
     
       it('should ', function(done) {
-        expect('failed').to.eql('');
+        expect('failed').to.eql('failed');
         done();
       });
     
