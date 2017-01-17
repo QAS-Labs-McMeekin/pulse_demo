2e4de70ba04555cc7a595445cf761ea66912b24d
Bobby Smith <bobby.smith@gmail.com>
2017-01-17 22:24:34 +0700

    Test Case Results for Untitled Test Case todos-1

---
+++ src/test-case-manager/test-run/Untitled Test Case todos-1.spec.ts
@@ -0,0 +1,20 @@
+
+      describe('Untitled Test Case todos-1', function(done) {
+        
+      it('should step', function(done) {
+        expect('result').to.eql('result');
+        done();
+      });
+    
+      it('should another step', function(done) {
+        expect('another result').to.eql('another result');
+        done();
+      });
+    
+      it('should ', function(done) {
+        expect('').to.eql('');
+        done();
+      });
+    
+      });
+    
