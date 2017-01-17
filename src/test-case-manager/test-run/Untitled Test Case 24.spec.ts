02c854c3384a02799b12271414013d6ee905758c
Bobby Smith <bobby.smith@gmail.com>
2017-01-17 22:04:19 +0700

    Test Case Results for Untitled Test Case 24

---
+++ src/test-case-manager/test-run/Untitled Test Case 24.spec.ts
@@ -0,0 +1,20 @@
+
+      describe('Untitled Test Case 24', function(done) {
+        
+      it('should Some description', function(done) {
+        expect('failed').to.eql('Some text');
+        done();
+      });
+    
+      it('should Another description', function(done) {
+        expect('failed').to.eql('Another text step');
+        done();
+      });
+    
+      it('should ', function(done) {
+        expect('failed').to.eql('');
+        done();
+      });
+    
+      });
+    
