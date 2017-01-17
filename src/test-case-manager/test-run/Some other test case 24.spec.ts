727732265e4c35deb64db0cd5ee0a7a467aecfc5
Bobby Smith <bobby.smith@gmail.com>
2017-01-17 23:28:28 +0700

    Test Case Results for Some other test case 24

---
+++ src/test-case-manager/test-run/Some other test case 24.spec.ts
@@ -0,0 +1,20 @@
+
+      describe('Some other test case 24', function(done) {
+        
+      it('should Step', function(done) {
+        expect('failed').to.eql('Result');
+        done();
+      });
+    
+      it('should Another Step', function(done) {
+        expect('failed').to.eql('Another Result');
+        done();
+      });
+    
+      it('should ', function(done) {
+        expect('failed').to.eql('');
+        done();
+      });
+    
+      });
+    
