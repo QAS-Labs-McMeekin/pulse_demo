16e3929c8e092fd45c9bdc873cc4ce2e82ac543f
Bobby Smith <bobby.smith@gmail.com>
2017-01-19 09:18:54 +0700

    Test Case Results for Add Jira Integration

---
+++ src/test-case-manager/test-run/Add Jira Integration.spec.ts
@@ -0,0 +1,35 @@
+
+      describe('Add Jira Integration', function(done) {
+        
+      it('should Go to Project settings', function(done) {
+        expect('failed').to.eql('undefined');
+        done();
+      });
+    
+      it('should Click on Add Integration', function(done) {
+        expect('failed').to.eql('undefined');
+        done();
+      });
+    
+      it('should Input Jira credentials', function(done) {
+        expect('failed').to.eql('undefined');
+        done();
+      });
+    
+      it('should Copy Pulse webhook to Jira', function(done) {
+        expect('failed').to.eql('undefined');
+        done();
+      });
+    
+      it('should Save Integration', function(done) {
+        expect('failed').to.eql('Integration work correctly');
+        done();
+      });
+    
+      it('should ', function(done) {
+        expect('failed').to.eql('');
+        done();
+      });
+    
+      });
+    
